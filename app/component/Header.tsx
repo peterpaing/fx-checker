3050ba1cbe37e002f10b079d8702e7c8730440d6
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-08-12 17:35:22 +0630

    feat: build responsive FX Checker header

---
+++ app/component/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header(){
+return (
+    <header className="w-full p-4 flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+        <svg  className="md:w-[26px]" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M1.49797 1.65034C2.47941 0.597411 3.88277 0 5.56017 0H14.4388C16.1204 0 17.5243 0.597189 18.5052 1.65054C19.482 2.69965 20 4.15844 20 5.81724V14.1827C20 15.8416 19.482 17.3003 18.5051 18.3495C17.524 19.4028 16.1199 20 14.4378 20H5.56017C3.87849 20 2.47485 19.4028 1.49427 18.3494C0.517663 17.3003 0 15.8415 0 14.1827V5.81724C0 4.15786 0.520358 2.69917 1.49797 1.65034ZM13.3181 6.93794C13.5819 6.59075 13.5143 6.09548 13.1672 5.83172C12.8199 5.56795 12.3246 5.63558 12.0608 5.98276L6.68188 13.0629C6.41813 13.4101 6.48575 13.9054 6.83293 14.1692C7.18012 14.4329 7.67539 14.3653 7.93916 14.0181L13.3181 6.93794Z" fill="#CEF739"/>
+        </svg>
+        <h1 className="text-sm md:text-base font-bold">FX_CHECKER</h1>
+        </div>
+        <ul className="flex items-center gap-1 md:gap-2 text-sm md:text-base uppercase text-neutral-400 whitespace-nowrap">
+        <li className="flex items-center gap-1 md:gap-2 after:content-['•']">55 Currencies</li>
+        <li className="flex items-center gap-1 md:gap-2 after:content-['•']">Eod</li>
+        <li className="flex items-center gap-1 md:gap-2">Ecb Data</li>
+        </ul>
+    </header>
+)
+}
