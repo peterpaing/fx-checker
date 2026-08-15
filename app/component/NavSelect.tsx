55bbafdf79ec4882c1587cac9ae60da3c67c8cff
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-08-16 00:10:18 +0630

    fix(navbar): sync mobile select with current route

--- app/component/NavSelect.tsx
+++ app/component/NavSelect.tsx
@@ -1,11 +1,18 @@
-import { MdArrowDropDown } from "react-icons/md";
+"use client"
+import { MdArrowDropDown } from "react-icons/md"
+import { usePathname, useRouter } from "next/navigation"
 
 export default function NavSelect(){
-    return (
+  const pathname = usePathname()
+  const router = useRouter()
+
+  return (
     <div className=" w-6/7 mx-auto mt-4 relative md:hidden">
     <select className="appearance-none w-full h-12 px-3 pr-8 rounded-lg border border-neutral-700 bg-neutral-900
      text-base uppercase outline-none leading-none
-     focus:outline-none focus:ring-2 focus:ring-lime-400 focus:ring-offset-black">
+     focus:outline-none focus:ring-2 focus:ring-lime-400"
+     value={pathname}
+      onChange={(e) => router.push(e.target.value)}>
     <option value="/" className="text-lg">HISTORY</option>
     <option value="/compare" className="text-lg">COMPARE</option>
     <option value="/favorites" className="text-lg">FAVORITES</option>
